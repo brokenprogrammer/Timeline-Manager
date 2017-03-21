5438ef8abead8c5db9270f86c2f1d87d82c1df7b
Brokenprogrammer <brokenprogrammer@gmail.com>
2017-03-21 15:10:28 +0100

    Added initial file structure

---
+++ src/com/timelinemanager/TimelineManager.java
@@ -0,0 +1,15 @@
+package com.timelinemanager;
+
+/**
+ * 
+ * @author Oskar Mendel
+ *
+ */
+public class TimelineManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
