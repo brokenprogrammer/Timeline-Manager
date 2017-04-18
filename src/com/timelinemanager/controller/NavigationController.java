4044a21e53629b82c207a5a54567a7dcfc2162e0
Brokenprogrammer <brokenprogrammer@gmail.com>
2017-04-18 12:46:09 +0200

    Connected navigationcontroller with its fxml

--- src/com/timelinemanager/controller/NavigationController.java
+++ src/com/timelinemanager/controller/NavigationController.java
@@ -1,5 +1,11 @@
 package com.timelinemanager.controller;
 
+import com.timelinemanager.model.TimelineModel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
 /**
  * Controls the actions for the UI events related to the
  * navigation area of the application.
@@ -10,7 +16,39 @@ package com.timelinemanager.controller;
  */
 public class NavigationController {
 	
+	@FXML private MenuBar navigationMenu;
+	
+	//Buttons within the File menu inside the MenuBar
+	@FXML private MenuItem menuItem_new;
+	@FXML private MenuItem menuItem_open;
+	@FXML private MenuItem menuItem_save;
+	@FXML private MenuItem menuItem_saveAs;
+	@FXML private MenuItem menuItem_close;
+	@FXML private MenuItem menuItem_exit;
+	
+	//Buttons within the Edit menu inside the MenuBar
+	@FXML private MenuItem menuItem_timeline;
+	@FXML private MenuItem menuItem_event;
+	
+	//Buttons within the Help menu inside the MenuBar
+	@FXML private MenuItem menuItem_about;
+	
+	private TimelineModel timelineModel;
+	
+	@FXML
 	public void initialize() {
 
+	}
+	
+	
+	public void initTimelineModel(TimelineModel timelineModel) {
+		if (this.timelineModel != null) {
+			throw new IllegalStateException("Model can only be initiated once");
+		}
+		
+		this.timelineModel = timelineModel;
+		
+		//TODO: Add potential listeners here
+		
 	}
 }
