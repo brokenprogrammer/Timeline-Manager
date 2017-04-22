6e8e68f5e87532928965d8514703611a66b6bc07
Brokenprogrammer <brokenprogrammer@gmail.com>
2017-04-22 17:24:14 +0200

    Added test classes for Timeline & Event.

---
+++ test/com/timelinemanager/Entity/EventTest.java
@@ -0,0 +1,184 @@
+package com.timelinemanager.Entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for Event.
+ * 
+ * @author Oskar Mendel
+ * @version 0.00.00
+ * @name EventTest.java
+ */
+public class EventTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#Event(java.lang.String, java.lang.String, java.time.LocalDate, java.time.LocalDate, java.time.LocalTime, java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testEventStringStringLocalDateLocalDateLocalTimeLocalTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#Event(java.lang.String, java.lang.String, java.lang.String, java.time.LocalDate, java.time.LocalDate, java.time.LocalTime, java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testEventStringStringStringLocalDateLocalDateLocalTimeLocalTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#Event(java.lang.String, java.lang.String, java.time.LocalDate, java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testEventStringStringLocalDateLocalTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#Event(java.lang.String, java.lang.String, java.lang.String, java.time.LocalDate, java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testEventStringStringStringLocalDateLocalTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setTitle(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setDescription(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setStartDate(java.time.LocalDate)}.
+	 */
+	@Test
+	public final void testSetStartDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setEndDate(java.time.LocalDate)}.
+	 */
+	@Test
+	public final void testSetEndDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setStartTime(java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testSetStartTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setEndTime(java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testSetEndTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#setPic(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetPic() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getTitle()}.
+	 */
+	@Test
+	public final void testGetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getDescription()}.
+	 */
+	@Test
+	public final void testGetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getStartDate()}.
+	 */
+	@Test
+	public final void testGetStartDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getEndDate()}.
+	 */
+	@Test
+	public final void testGetEndDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getStartTime()}.
+	 */
+	@Test
+	public final void testGetStartTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getEndTime()}.
+	 */
+	@Test
+	public final void testGetEndTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#getPic()}.
+	 */
+	@Test
+	public final void testGetPic() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Event#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ test/com/timelinemanager/Entity/TimelineTest.java
@@ -0,0 +1,216 @@
+package com.timelinemanager.Entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for Timeline.
+ * 
+ * @author Oskar Mendel
+ * @version 0.00.00
+ * @name TimelineTest.java
+ */
+public class TimelineTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#Timeline()}.
+	 */
+	@Test
+	public final void testTimeline() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#Timeline(java.lang.String, java.lang.String, java.lang.String, java.time.LocalDate, java.time.LocalDate, java.time.LocalTime, java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testTimelineStringStringStringLocalDateLocalDateLocalTimeLocalTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#Timeline(java.lang.String, java.lang.String, java.time.LocalDate, java.time.LocalDate, java.time.LocalTime, java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testTimelineStringStringLocalDateLocalDateLocalTimeLocalTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setTitle(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setDescription(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setStartDate(java.time.LocalDate)}.
+	 */
+	@Test
+	public final void testSetStartDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setEndDate(java.time.LocalDate)}.
+	 */
+	@Test
+	public final void testSetEndDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setStartTime(java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testSetStartTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setEndTime(java.time.LocalTime)}.
+	 */
+	@Test
+	public final void testSetEndTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#setPic(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetPic() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getTitle()}.
+	 */
+	@Test
+	public final void testGetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getDescription()}.
+	 */
+	@Test
+	public final void testGetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getStartDate()}.
+	 */
+	@Test
+	public final void testGetStartDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getEndDate()}.
+	 */
+	@Test
+	public final void testGetEndDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getStartTime()}.
+	 */
+	@Test
+	public final void testGetStartTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getEndTime()}.
+	 */
+	@Test
+	public final void testGetEndTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getPic()}.
+	 */
+	@Test
+	public final void testGetPic() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#addEvent(com.timelinemanager.Entity.Event)}.
+	 */
+	@Test
+	public final void testAddEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#deleteEvent(com.timelinemanager.Entity.Event)}.
+	 */
+	@Test
+	public final void testDeleteEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getDaysLength()}.
+	 */
+	@Test
+	public final void testGetDaysLength() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getMonthsLength()}.
+	 */
+	@Test
+	public final void testGetMonthsLength() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#getYearsLength()}.
+	 */
+	@Test
+	public final void testGetYearsLength() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.timelinemanager.Entity.Timeline#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
