c10fe660438023eda64d118d9b88605a47f2184d
miiro19 <mm223dz@student.lnu.se>
2017-04-26 08:39:57 +0200

    Fixed actionEvent for "Cancel" button
    * Opens a pop-up window asking for confirmation
    * Added necessary imports

--- src/com/timelinemanager/controller/CreateEventController.java
+++ src/com/timelinemanager/controller/CreateEventController.java
@@ -2,6 +2,7 @@ package com.timelinemanager.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 import com.timelinemanager.Entity.Timeline;
 import com.timelinemanager.model.TimelineModel;
@@ -9,7 +10,9 @@ import com.timelinemanager.model.TimelineModel;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -18,6 +21,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 
 /**
  * 
@@ -97,8 +101,26 @@ public class CreateEventController {
 
 		// ActionEvent for the cancel button inside the "Create new event"
 		// window
+		//Opens a pop-up window asking for exit confirmation
 		cancelCreateEvent.setOnAction(cancelEvent -> {
-			((Node) (cancelEvent.getSource())).getScene().getWindow().hide();
+			
+			Alert closeConfirmation = new Alert(
+	                Alert.AlertType.CONFIRMATION,
+	                "Are you sure you want to cancel creating an event?"
+	        );
+			cancelCreateEvent = (Button) closeConfirmation.getDialogPane().lookupButton(
+	                ButtonType.OK
+	        );
+	        closeConfirmation.setHeaderText("Confirm Exit");
+	        closeConfirmation.initModality(Modality.APPLICATION_MODAL);
+	        Optional<ButtonType> closeResponse = closeConfirmation.showAndWait();
+	        if (!ButtonType.OK.equals(closeResponse.get())) {
+	        	cancelEvent.consume();
+	        }
+	        else
+	        {
+	        	((Node)(cancelEvent.getSource())).getScene().getWindow().hide();
+	        }
 		});
 	}
 
