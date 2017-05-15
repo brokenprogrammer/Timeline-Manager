e77fff5b03afcf7060de7a4eb46e169a1a9cd2e5
dargiriou <dargiriou@gmail.com>
2017-05-15 16:23:05 +0200

    Update StageManager.java

--- src/com/timelinemanager/StageManager.java
+++ src/com/timelinemanager/StageManager.java
@@ -50,6 +50,7 @@ public class StageManager {
 		rootLayout.setCenter(timelineLayout);
 		
 		Scene mainScene = new Scene(rootLayout);
+		mainScene.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 		mainStage.setOnCloseRequest(onExitCloseEverything -> System.exit(0));
 		mainStage.setScene(mainScene);
 		mainStage.setMinWidth(800);
