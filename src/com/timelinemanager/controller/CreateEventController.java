f186713aee8de52f2ad6ed126427f1eb819613d1
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-18 01:19:53 +0200

    Fixed adding of events without duration.

--- src/com/timelinemanager/controller/CreateEventController.java
+++ src/com/timelinemanager/controller/CreateEventController.java
@@ -127,14 +127,14 @@ public class CreateEventController {
 				alert.setContentText("The date is outside the timeline range! Please check start date.");
 				alert.showAndWait();
 				
-			} else if (this.timelineModel.getTimeline().getValue().getEndDate() != null 
-				  && (this.timelineModel.getTimeline().getValue().getEndDate()).isBefore(endEvent)) {
-				Alert alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error Dialog");
-				alert.setHeaderText("Date Error");
-				alert.setContentText("The date is outside the timeline range! Please check end date.");
-				alert.showAndWait();
-				
+			} else if (this.timelineModel.getTimeline().getValue().getEndDate() != null && (endEvent != null)) {
+				if (this.timelineModel.getTimeline().getValue().getEndDate().isBefore(endEvent)) {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Error Dialog");
+					alert.setHeaderText("Date Error");
+					alert.setContentText("The date is outside the timeline range! Please check end date.");
+					alert.showAndWait();
+				}
 			} else {
 				// Populate event object with data
 				Event newEvent = new Event();
