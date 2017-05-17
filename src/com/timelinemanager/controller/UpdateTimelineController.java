968e98122522ace3c3851e883b7bbda4349a3666
dargiriou <dargiriou@gmail.com>
2017-05-18 01:50:38 +0200

    alert ids again

--- src/com/timelinemanager/controller/UpdateTimelineController.java
+++ src/com/timelinemanager/controller/UpdateTimelineController.java
@@ -1,10 +1,8 @@
 package com.timelinemanager.controller;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Optional;
 
-import com.timelinemanager.Entity.Event;
 import com.timelinemanager.Entity.Timeline;
 import com.timelinemanager.model.TimelineModel;
 
@@ -15,6 +13,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
@@ -65,6 +64,8 @@ public class UpdateTimelineController {
 
 			if (updateTimelineTitle.getText().length() == 0 && start == null && end == null) {
 				Alert alert = new Alert(AlertType.ERROR);
+				DialogPane dialogPane = alert.getDialogPane();
+				dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 				alert.setTitle("Error Dialog");
 				alert.setHeaderText("Input Error");
 				alert.setContentText("The required fields are empty, please fill them to create a Timeline!");
@@ -72,6 +73,8 @@ public class UpdateTimelineController {
 				
 			} else if (updateTimelineTitle.getText().length() == 0) {
 				Alert alert = new Alert(AlertType.ERROR);
+				DialogPane dialogPane = alert.getDialogPane();
+				dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 				alert.setTitle("Error Dialog");
 				alert.setHeaderText("Input Error");
 				alert.setContentText("Title is missing, please enter a title to create a Timeline!");
@@ -79,6 +82,8 @@ public class UpdateTimelineController {
 				
 			} else if (start == null || end == null) {
 				Alert alert = new Alert(AlertType.ERROR);
+				DialogPane dialogPane = alert.getDialogPane();
+				dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 				alert.setTitle("Error Dialog");
 				alert.setHeaderText("Date Error");
 				alert.setContentText("Date is missing, please enter a start and end date to create a Timeline!");
@@ -86,6 +91,8 @@ public class UpdateTimelineController {
 				
 			} else if (updateTimelineTitle.getText().length() > 50) {
 				Alert alert = new Alert(AlertType.ERROR);
+				DialogPane dialogPane = alert.getDialogPane();
+				dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 				alert.setTitle("Error Dialog");
 				alert.setHeaderText("Input Error");
 				alert.setContentText("Max 50 characters only!");
@@ -93,6 +100,8 @@ public class UpdateTimelineController {
 				
 			} else if (updateTimelineDescription.getText().length() > 500) {
 				Alert alert = new Alert(AlertType.ERROR);
+				DialogPane dialogPane = alert.getDialogPane();
+				dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 				alert.setTitle("Error Dialog");
 				alert.setHeaderText("Input Error");
 				alert.setContentText("Max 500 characters only!");
@@ -100,50 +109,16 @@ public class UpdateTimelineController {
 				
 			} else if (start.isAfter(end)) {
 				Alert alert = new Alert(AlertType.ERROR);
+				DialogPane dialogPane = alert.getDialogPane();
+				dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 				alert.setTitle("Error Dialog");
 				alert.setHeaderText("Date Error");
 				alert.setContentText("Please type date in the correct date format!");
 				alert.showAndWait();
 				
 			} else {
-				boolean eventsChecked = true;
-				Timeline t = new Timeline(updateTimelineTitle.getText(), updateTimelineDescription.getText(),
-						start, end, LocalTime.now(), LocalTime.now().plusHours(1));
-				
-				// Check so that all events are within the timespan of the new timeline.
-				for (Event e : this.timelineModel.getTimeline().getValue().eventArr) {
-					if (e.getStartDate().isBefore(t.getStartDate()) || e.getStartDate().isAfter(t.getEndDate())) {
-						eventsChecked = false;
-						
-						Alert alert = new Alert(AlertType.ERROR);
-						alert.setTitle("Error Dialog");
-						alert.setHeaderText("Date Error");
-						alert.setContentText("All the events start dates are not within the timelines timespan!");
-						alert.showAndWait();
-					} else if (e.getEndDate().isBefore(t.getStartDate()) || e.getEndDate().isAfter(t.getEndDate())) {
-						eventsChecked = false;
-						
-						Alert alert = new Alert(AlertType.ERROR);
-						alert.setTitle("Error Dialog");
-						alert.setHeaderText("Date Error");
-						alert.setContentText("All the events end dates are not within the timelines timespan!");
-						alert.showAndWait();
-					}
-				}
-				
-				
-				if (eventsChecked) {
-					// Add all the events to the newly constructed timeline.
-					for (Event e : this.timelineModel.getTimeline().getValue().eventArr) {
-						t.addEvent(e);
-					}
-					
-					// Update the model with the new timeline and update the list with loaded timelines as well.
-					this.timelineModel.setTimeline(t);
-					int indx = this.timelineModel.getLoadedTimelines().indexOf(updatedTimeline);
-					this.timelineModel.getLoadedTimelines().set(indx, t);
-					((Node) (ev.getSource())).getScene().getWindow().hide();
-				}
+				this.timelineModel.setTimeline(updatedTimeline);
+				((Node) (ev.getSource())).getScene().getWindow().hide();
 			}
 		});
 
@@ -154,6 +129,8 @@ public class UpdateTimelineController {
 
 			Alert closeConfirmation = new Alert(Alert.AlertType.CONFIRMATION,
 					"Are you sure you want to cancel editing the timeline?");
+			DialogPane dialogPane = closeConfirmation.getDialogPane();
+			dialogPane.getStylesheets().add(getClass().getResource("/view/style.css").toString());
 			updateCancelEditTimeline = (Button) closeConfirmation.getDialogPane().lookupButton(ButtonType.OK);
 			closeConfirmation.setHeaderText("Confirm Exit");
 			closeConfirmation.initModality(Modality.APPLICATION_MODAL);
