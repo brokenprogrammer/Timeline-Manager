d012e3324293b350171a7471a79d213a595dd9b7
dargiriou <dargiriou@gmail.com>
2017-05-19 13:46:58 +0200

    ids again, also for alerts

--- src/com/timelinemanager/controller/TimelineListingController.java
+++ src/com/timelinemanager/controller/TimelineListingController.java
@@ -35,11 +35,11 @@ public class TimelineListingController {
 
 	@FXML
 	public void initialize() {
-		timelineListing_anchorpane.setPrefSize(160, 500);
-		timelineListing_anchorpane.setMinSize(160, 500);
+		timelineListing_anchorpane.setPrefSize(160, 480);
+		timelineListing_anchorpane.setMinSize(160, 480);
 		timelineListing_anchorpane.getChildren().add(list);
-		list.setPrefSize(160, 500);
-		list.setMinSize(160, 500);
+		list.setPrefSize(160, 480);
+		list.setMinSize(160, 480);
 	}
 
 	/**
