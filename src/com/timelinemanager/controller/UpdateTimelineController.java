4517d9ae3ef9431f1c2881d32a4099c27381861f
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-23 09:19:32 +0200

    Fixed errors of missing imports after merge.

--- src/com/timelinemanager/controller/UpdateTimelineController.java
+++ src/com/timelinemanager/controller/UpdateTimelineController.java
@@ -18,9 +18,11 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * 
